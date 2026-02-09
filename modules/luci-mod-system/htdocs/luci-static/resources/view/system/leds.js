ccf6764174cdb59d8392ec750fc49294fb22f27d
Tomáš Macholda <tomas.macholda@nic.cz>
2026-02-09 15:11:39 +0100

    luci-mod-system: support for RGB LEDs
    added support for setting LED brightness and colours
    
    channels and max_brightness are pulled dynamically from sysfs
    
    Signed-off-by: Tomáš Macholda <tomas.macholda@nic.cz>

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/leds.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/leds.js
@@ -107,6 +107,26 @@ return view.extend({
 				var plugin = plugins[i];
 				plugin.form.addFormOptions(s);
 			}
+			var led_names = Object.keys(leds)
+			for (var i = 0; i < led_names.length; i++) {
+				var led = leds[led_names[i]];
+				o = s.option(form.Value, `${led_names[i]}_brightness`, _('Brightness'));
+				o.depends('sysfs', led_names[i])
+				o.ucioption = 'brightness'
+				o.datatype = `range(0,${led.max_brightness})`;
+				o.placeholder = led.max_brightness;
+				o.modalonly = true;
+				if (led.multi_channel) {
+					for (var channel in led.channels) {
+						o = s.option(form.Value, `${led_names[i]}_color_${channel}`, channel);
+						o.ucioption = `color_${channel}`
+						o.depends('sysfs', led_names[i])
+						o.datatype = `range(0,${led.max_brightness})`;
+						o.placeholder = led.channels[channel];
+						o.modalonly = true;
+					}
+				}
+			}
 
 			var opts = s.getOption();
 
